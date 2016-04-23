720103d2c2c6e41b5173f8ddb8fc7a7d4d50d6d0
Val Booth <vxb4825@rit.edu>
2016-04-23 17:43:09 -0400

    Attempting to load image from the internet, currently just a place kitten.

--- app/src/main/java/me/littlecabbage/androidspacescenes/SimpleWidgetProvider.java
+++ app/src/main/java/me/littlecabbage/androidspacescenes/SimpleWidgetProvider.java
@@ -5,20 +5,33 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
 
 public class SimpleWidgetProvider extends AppWidgetProvider {
-
+    ImageView imageView;
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int widgetId : appWidgetIds) {
 //            String number = String.format("%03d", (new Random().nextInt(900) + 100));
-//
-//            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.simple_widget);
-//
-//            remoteViews.setTextViewText(R.id.textView, number);
+//      imageView = (ImageView) findViewById(R.id.imageView);
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.simple_widget);
+            try {
+                remoteViews.setImageViewBitmap(R.id.imageView, bitmapFromUrl("http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg"));
+            } catch (IOException e) {
+                //TODO error message
+                e.printStackTrace();
+            }
 
             Intent intent = new Intent(context, SimpleWidgetProvider.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
@@ -28,5 +41,16 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
 //            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
 //            appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
+    }
+
+    public static Bitmap bitmapFromUrl(String url) throws IOException {
+        Bitmap x;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.connect();
+        InputStream input = connection.getInputStream();
+
+        x = BitmapFactory.decodeStream(input);
+        return x;
     }
 }
