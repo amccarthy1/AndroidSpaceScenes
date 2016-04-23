766ae056154ec3abad86ed777f12cef1e9c874b6
Adam McCarthy <amccarthy@mail.rit.edu>
2016-04-23 15:10:19 -0400

    Remove unnecessary portions of the widget

--- app/src/main/java/me/littlecabbage/androidspacescenes/SimpleWidgetProvider.java
+++ app/src/main/java/me/littlecabbage/androidspacescenes/SimpleWidgetProvider.java
@@ -14,19 +14,19 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int widgetId : appWidgetIds) {
-            String number = String.format("%03d", (new Random().nextInt(900) + 100));
-
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.simple_widget);
-
-            remoteViews.setTextViewText(R.id.textView, number);
+//            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+//
+//            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.simple_widget);
+//
+//            remoteViews.setTextViewText(R.id.textView, number);
 
             Intent intent = new Intent(context, SimpleWidgetProvider.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
                     0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
-            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+//            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+//            appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
 }
