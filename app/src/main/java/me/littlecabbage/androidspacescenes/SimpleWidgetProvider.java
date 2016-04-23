5b06769d3491f8b59d3d6f2a327e682c7a4620e9
Adam McCarthy <amccarthy@mail.rit.edu>
2016-04-23 19:52:10 -0400

    DOCUMENTATION! :D

--- app/src/main/java/me/littlecabbage/androidspacescenes/SimpleWidgetProvider.java
+++ app/src/main/java/me/littlecabbage/androidspacescenes/SimpleWidgetProvider.java
@@ -24,14 +24,20 @@ import java.io.InputStream;
 import java.net.URL;
 
 public class SimpleWidgetProvider extends AppWidgetProvider {
+    //To be used as a callback function to reset the widget with the loaded image
+    private interface CallBack<T> {
+        public void run(T arg);
+    }
+
     @Override
     public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
         Toast.makeText(context, "Widget Updating", Toast.LENGTH_LONG).show();
         for (final int widgetId : appWidgetIds) {
             final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.simple_widget);
-            // FIXME: 4/23/2016
+            // FIXME: 4/23/2016, remove key, final variables
             String key = "CD4cC9NEeIUmK8bufx1hvsShTY25RmzVlw2JXA2L";
             String requestUrl = "https://api.nasa.gov/planetary/apod?api_key=" + key;
+            //Get the current photo of the dat data
             JsonObjectRequest jsObjRequest = new JsonObjectRequest
                     (Request.Method.GET, requestUrl, null, new Response.Listener<JSONObject>() {
 
@@ -41,7 +47,24 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
                             try {
                                 Object u = response.get("url");
                                 String photoUrl = (String)u;
-                                new LoadImage(remoteViews, context, appWidgetIds, appWidgetManager, widgetId).execute(photoUrl);
+                                new LoadImage(new CallBack<Bitmap>() {
+                                    //Once we have gone to the image URL and converted it to a Bitmap,
+                                    //we need to update the app with the new pretty bitmap.
+                                    //This code, while ugly, is less coupled than previously.
+                                    @Override
+                                    public void run(Bitmap image) {
+                                        if (image != null) {
+                                            remoteViews.setImageViewBitmap(R.id.imageView, image);
+                                        }
+                                        Intent intent = new Intent(context, SimpleWidgetProvider.class);
+                                        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                                        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+                                        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                                                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                                        remoteViews.setOnClickPendingIntent(R.id.imageView, pendingIntent);
+                                        appWidgetManager.updateAppWidget(widgetId, remoteViews);
+                                    }
+                                }).execute(photoUrl);
 
                             } catch (JSONException e) {
                                 Toast.makeText(context, ":(", Toast.LENGTH_LONG).show();
@@ -51,7 +74,6 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
 
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            // TODO Auto-generated method stub
                             Toast.makeText(context, "Failed to get a valid JSON response", Toast.LENGTH_LONG).show();
                         }
                     });
@@ -59,47 +81,31 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
         }
     }
 
-    //p
-
+    //Goes to the URL given to doInBackground in its args, creates a bitmap from its
+    //input stream, and once it's loaded, updates the app using the callback function
+    //given to its constructor.
     private class LoadImage extends AsyncTask<String, String, Bitmap> {
         private Bitmap bitmap;
-        private RemoteViews remoteViews;
-        private Context context;
-        private int[] appWidgetIds;
-        private AppWidgetManager appWidgetManager;
-        private int id;
+        private CallBack<Bitmap> cb;
 
-        public LoadImage(RemoteViews remoteViews, Context context, int[] appWidgetIds, AppWidgetManager appWidgetManager, int id) {
+        //Load Image takes a callback function to update the app
+        public LoadImage(CallBack<Bitmap> cb) {
             super();
-            this.remoteViews = remoteViews;
-            this.context = context;
-            this.appWidgetIds = appWidgetIds;
-            this.appWidgetManager = appWidgetManager;
-            this.id = id;
+            this.cb = cb;
         }
+
         protected Bitmap doInBackground(String... args) {
             try {
                 bitmap = BitmapFactory.decodeStream((InputStream)new URL(args[0]).getContent());
 
             } catch (Exception e) {
-                Toast.makeText(context, "failed to decode URL", Toast.LENGTH_LONG).show();
                 e.printStackTrace();
             }
             return bitmap;
         }
 
         protected void onPostExecute(Bitmap image) {
-
-            if (image != null) {
-                this.remoteViews.setImageViewBitmap(R.id.imageView, image);
-            }
-            Intent intent = new Intent(context, SimpleWidgetProvider.class);
-            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
-                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.imageView, pendingIntent);
-            appWidgetManager.updateAppWidget(id, remoteViews);
+            cb.run(image);
         }
     }
 }
